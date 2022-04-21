250b5cafb183c394d81449cb8b77b8684adc82a2
Lucas Cavalcanti <lucas-cavalcanti@live.com>
2022-04-21 10:21:45 -0300

    Primeiro contato com o IDE

---
+++ Aula 01 - Olá,mundo.java
@@ -0,0 +1,16 @@
+package primeiroprograma;
+
+/**
+ *
+ * @author lucas
+ */
+public class PrimeiroPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Olá,Mundo");
+    }
+    
+}
