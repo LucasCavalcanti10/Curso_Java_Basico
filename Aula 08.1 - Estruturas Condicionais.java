0d0c95968fb11695f7302aa2a30a40480db37e50
Lucas Cavalcanti <lucas-cavalcanti@live.com>
2022-05-12 18:44:23 -0300

    Utilizando If e Else no netbeans

---
+++ Aula 08.1 - Estruturas Condicionais.java
@@ -0,0 +1,34 @@
+package programaidade;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lucas
+ */
+public class ProgramaIdade {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner t = new Scanner(System.in);
+        System.out.print("Em que ano você nasceu: ");
+        int nasc = t.nextInt();
+        int idade = 2022 - nasc;
+        System.out.println("Sua idade é " + idade );
+        if (idade >= 18) {
+            System.out.println("Maior de idade");
+        } else {
+            System.out.println("Menor de idade");
+        }
+            
+        
+           
+        
+        
+        
+        
+    }
+    
+}
